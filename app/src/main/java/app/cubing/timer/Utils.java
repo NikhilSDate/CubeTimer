769dd02300319c7ec7ccec51bf92a7b0b3b3e8dc
BlackCubing <nikhil.s.date@gmail.com>
2019-03-01 20:57:31 +0530

    test

--- app/src/main/java/app/cubing/timer/Utils.java
+++ app/src/main/java/app/cubing/timer/Utils.java
@@ -136,7 +136,7 @@ public class Utils {
         }
         public static Integer[] getPuzzleDrawableIds(){
             return new Integer[]{R.drawable.ic_2x2,R.drawable.ic_3x3,R.drawable.ic_4x4,R.drawable.ic_5x5
-                    ,R.drawable.ic_6x6,R.drawable.ic_7x7,R.drawable.ic_prya,R.drawable.ic_skewb};
+                    ,R.drawable.ic_6x6,R.drawable.ic_7x7,R.drawable.ic_prya,R.drawable.ic_skewb,R.drawable.ic_mega};
         }
 }
 
